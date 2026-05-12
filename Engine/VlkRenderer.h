fd7154c4a2188c07144cb71a2b9e4f67fb15d2e2
Jovancic <aleksandar.jovancic@hotbytes.de>
2026-05-12 14:32:43 +0200

    Remove circular dependencies in terms of pointers and shit.

--- Engine/VlkRenderer.h
+++ Engine/VlkRenderer.h
@@ -15,8 +15,6 @@
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 
-class Game;
-
 class VlkRenderer {
 public:
     // Variables
@@ -79,10 +77,6 @@ public:
     // Initial Indices
     const std::vector<uint32_t> indices = {0, 1, 2, 2, 3, 0};
     std::unordered_map<std::string, MeshBuffer> meshes;
-
-    Game *game = nullptr;
-
-    void setGame(Game &g) { game = &g; }
 
     // Vulkan Datatype Variables
     VkInstance instance;
