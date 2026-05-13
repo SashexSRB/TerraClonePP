dab6252f79311323f6e4f8cc1f26abd31672da07
Jovancic <aleksandar.jovancic@hotbytes.de>
2026-05-13 08:32:51 +0200

    Reformat Vertex Code

--- Engine/Vertex.h
+++ Engine/Vertex.h
@@ -19,8 +19,7 @@ struct Vertex {
         return bindingDescription;
     }
 
-    static std::array<VkVertexInputAttributeDescription, 4>
-    getAttributeDescriptions() {
+    static std::array<VkVertexInputAttributeDescription, 4> getAttributeDescriptions() {
         std::array<VkVertexInputAttributeDescription, 4> attributeDescriptions{};
 
         attributeDescriptions[0].binding = 0;
